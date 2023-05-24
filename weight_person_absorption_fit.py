8546e150fd2e0813802bfffd91ba4412de92ff70
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-05-24 11:16:10 +0200

    add aD and pD

--- weight_person_absorption_fit.py
+++ weight_person_absorption_fit.py
@@ -3,6 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+##################################################
+################## WEIGHT VS AD ##################
+##################################################
+
+df = pd.read_excel("weight_person_absorption.xlsx", sheet_name="Munka1")
+X = df["weight (kg)"].values.reshape(-1, 1)
+y = df["min aD (dB)"].values
+
+regressor = LinearRegression()
+regressor.fit(X, y)
+
+plt.scatter(X, y)
+plt.plot(X, regressor.predict(X), color='red')
+plt.xlabel('weight (kg)')
+plt.ylabel('min aD (dB)')
+plt.title('Linear Regression Fit')
+plt.text(0.7, 0.7, f"Slope = {regressor.coef_[0]:.2f} dB/kg\nIntercept = {regressor.intercept_:.2f} dB\n$R^2$ = {regressor.score(X,y):.2f}", transform=plt.gca().transAxes)
+plt.savefig("weight_person_absorption_fit.png")
+
+
+##################################################
+################## MEAN PD VS AD #################
+##################################################
+
 df = pd.read_excel("weight_person_absorption.xlsx", sheet_name="Munka2")
 X = df["mean aD (dB)"].values.reshape(-1, 1)
 y = df["mean pD"].values
@@ -10,10 +34,11 @@ y = df["mean pD"].values
 regressor = LinearRegression()
 regressor.fit(X, y)
 
+plt.figure()
 plt.scatter(X, y)
 plt.plot(X, regressor.predict(X), color='red')
 plt.xlabel('mean aD (dB)')
 plt.ylabel('mean pD')
 plt.title('Linear Regression Fit')
-plt.text(0.2, 0.7, f"Slope = {regressor.coef_[0]:.2f}\nIntercept = {regressor.intercept_:.2f}\n$R^2$ = {regressor.score(X,y):.2f}", transform=plt.gca().transAxes)
-plt.savefig("weight_person_absorption_fit.png")
+plt.text(0.2, 0.7, f"Slope = {regressor.coef_[0]:.2f} 1/dB\nIntercept = {regressor.intercept_:.2f}\n$R^2$ = {regressor.score(X,y):.2f}", transform=plt.gca().transAxes)
+plt.savefig("weight_person_absorption_fit_pd.png")
